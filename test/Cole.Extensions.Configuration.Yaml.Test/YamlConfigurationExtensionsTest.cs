6920aca42cf98c3fcd339e4357d03e79d0038c50
Cole <564603061@qq.com>
2023-01-16 21:44:13 +0800

    添加单元测试

---
+++ test/Cole.Extensions.Configuration.Yaml.Test/YamlConfigurationExtensionsTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+using Microsoft.Extensions.Configuration;
+
+namespace Cole.Extensions.Configuration.Yaml.Test;
+
+public class YamlConfigurationExtensionsTest
+{
+    public YamlConfigurationExtensionsTest()
+    {
+        Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en");
+    }
+
+    [Fact]
+    public void AddYamlFile_ThrowIfConfigurationBuilderIsNull()
+    {
+        // Arrange
+        IConfigurationBuilder builder = null;
+        string path = string.Empty;
+
+        // Act and Assert
+        var ex = Assert.Throws<ArgumentNullException>(() => builder.AddYamlFile(path));
+        Assert.Equal("builder", ex.ParamName);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void AddYamlFile_ThrowIfFilePathIsNullOrEmpty(string path)
+    {
+        // Arrange
+        IConfigurationBuilder builder = new ConfigurationBuilder();
+
+        // Act and Assert
+        var ex = Assert.Throws<ArgumentException>(() => builder.AddYamlFile(path));
+        Assert.Equal("path", ex.ParamName);
+        Assert.StartsWith("File path must be a non-empty string.", ex.Message);
+    }
+
+    [Fact]
+    public void AddYamlFile_ThrowIfFileDoesNotExist()
+    {
+        // Arrange
+        IConfigurationBuilder builder = new ConfigurationBuilder();
+        string path = "non-existent file.yml";
+
+        // Act and Assert
+        Assert.Throws<FileNotFoundException>(() => builder.AddYamlFile(path).Build());
+    }
+
+    [Fact]
+    public void AddYamlFile_NotThrowIfFileDoesNotExistAndIsOptional()
+    {
+        // Arrange
+        IConfigurationBuilder builder = new ConfigurationBuilder();
+        string path = "non-existent file.yml";
+        bool optional = true;
+
+        // Act and Assert
+        builder.AddYamlFile(path, optional).Build();
+    }
+}
